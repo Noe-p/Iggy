d81e7841dad2daa3003336fd8b21b92648ee1aac
Noé <noe.philippe@bob-le-developpeur.com>
2022-12-08 14:02:06 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className='App'>
       <Header />
       <Hero />
-      <Title anchor='feature'>Iggy vous aides à ... </Title>
+      <Title anchor='feature'>Iggy vous aide à ... </Title>
       <Mockup1 />
       <CallToAction />
       <Stat />
