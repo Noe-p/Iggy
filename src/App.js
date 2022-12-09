f14d90b316b8105e3e2b53bd7ada9fa9bb632d1b
Dorian Longépée <dorianlongepee.pro@gmail.com>
2022-12-09 11:20:58 +0100

    change form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Widget } from '@typeform/embed-react';
 import TagManager from 'react-gtm-module'
+import HubspotForm from 'react-hubspot-form'
 
 
 import './App.css';
@@ -25,13 +26,22 @@ function App() {
     <div className='App'>
       <Header />
       <Hero />
-      <Title anchor='feature'>Iggy vous aide à ... </Title>
+      <Title anchor='feature'>Iggy vous aide à ...</Title>
       <Mockup1 />
       <CallToAction />
       <Stat />
       <Testimonials />
-      <div id='typeform'>
-        <Widget id='LCa7UPQJ' height={400} disableScroll={true}></Widget>
+      <div className="flex justify-center flex-col content-center w-3/4 lg:w-1/2 mx-auto">
+      <Title anchor='contact'>Testez Iggy en avant-première</Title>
+        <div className='my-5 lg:my-0'>
+        <HubspotForm
+          portalId='26697104'
+          formId='c710d628-fb8b-43bd-b160-31d0838865a2'
+          onSubmit={() => console.log('Submit!')}
+          onReady={(form) => console.log('Form ready!')}
+          loading={<div>Loading...</div>}
+        />
+        </div>
       </div>
       <Footer />
     </div>
